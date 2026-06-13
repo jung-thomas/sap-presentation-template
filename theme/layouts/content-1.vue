82b914d7e71548971532b79ca29b3610aafa0a59
Thomas Jung <12159356+jung-thomas@users.noreply.github.com>
2026-06-13 07:45:10 -0400

    feat(layout): title, content-1, two-content (POTX-named aliases)

---
+++ theme/layouts/content-1.vue
@@ -0,0 +1,8 @@
+<template>
+  <TitleContent v-bind="$attrs"><slot /></TitleContent>
+</template>
+
+<script setup lang="ts">
+  import TitleContent from './title-content.vue'
+  defineOptions({ inheritAttrs: false })
+</script>
---
+++ theme/layouts/two-content.vue
@@ -0,0 +1,8 @@
+<template>
+  <TitleText2Col v-bind="$attrs"><slot /></TitleText2Col>
+</template>
+
+<script setup lang="ts">
+  import TitleText2Col from './title-text-2col.vue'
+  defineOptions({ inheritAttrs: false })
+</script>
