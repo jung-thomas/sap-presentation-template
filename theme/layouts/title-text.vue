372e83c8f02ef7de16bec69a9888f780c2a45ac4
Thomas Jung <12159356+jung-thomas@users.noreply.github.com>
2026-06-13 07:41:55 -0400

    feat(layout): title-text

---
+++ theme/layouts/title-text.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  defineProps<{ title?: string }>()
+</script>
+
+<template>
+  <div class="layout title-text">
+    <h1 v-if="title">{{ title }}</h1>
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .title-text {
+    padding: 3rem 4rem;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .title-text h1 {
+    font-size: 2.75rem;
+    color: var(--sap-brand-blue-darker);
+    margin-bottom: 1.5rem;
+  }
+  .title-text .content {
+    flex: 1;
+    font-size: 1.25rem;
+    line-height: 1.55;
+  }
+</style>
