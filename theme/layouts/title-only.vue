38340adaca5ffbdb0b692f1224f8019d74ed1c2a
Thomas Jung <12159356+jung-thomas@users.noreply.github.com>
2026-06-13 07:41:54 -0400

    feat(layout): title-only

---
+++ theme/layouts/title-only.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  defineProps<{ title?: string }>()
+</script>
+
+<template>
+  <div class="layout title-only">
+    <h1 v-if="title">{{ title }}</h1>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .title-only {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 4rem 5rem;
+    height: 100%;
+  }
+  .title-only h1 {
+    font-size: 4rem;
+    color: var(--sap-brand-blue-darker);
+  }
+</style>
